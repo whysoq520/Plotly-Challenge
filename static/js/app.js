0c11439662b48efdc20791ff51b02bcb2a3333fe
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-01-22 07:31:55 -0500

    dropdownMenu done

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,24 @@
 const path = "../../samples.json";
-const data = d3.json(path).then((data)=>{
-    console.log(data);
-    const names = data.names;
-    console.log(names);
-    console.log(data.samples)
+// dropdown
 
-});
+
+
+// Assign the dropdownmenu option to a variable
+var dropdownMenu = d3.select("#selDataset");
+const data = d3.json(path).then((data)=>{
+    //console.log(data);
+    const names = data.names;
+    //console.log(names);
+    names.forEach(ID => { 
+        //console.log(ID);
+        var option = dropdownMenu.append("option");
+        option.text(ID);
+                        
+                        });
+           
+    });
+    
+
 
 
 // 
