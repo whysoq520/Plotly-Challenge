166669ba62c81c3edfb35831459c55903a2ca971
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-01-22 18:11:42 -0500

    how to built function ?

--- static/js/app.js
+++ static/js/app.js
@@ -1,53 +1,23 @@
-const path = "../../samples.json";
+var path ="../../samples.json";
 
-// Assign variables to all selections
+// dropdown
 var dropdownMenu = d3.select("#selDataset");
-var demographic =d3.select("#sample-metadata");
-var demographic =d3.select("#bar");
-var demographic =d3.select("#gauge");
-var demographic =d3.select("#bubble");
-var demographic =d3.select("#sample-metadata")
-
-
-const data = d3.json(path).then((data)=>{
-    //console.log(data);
-
-    // dropdown
+const data = d3.json(path).then(function(data) {
+    //console.log(data);    
     const names = data.names;
     //console.log(names);
     names.forEach(ID => { 
-        //console.log(ID);
-        var option = dropdownMenu.append("option");
-        option.text(ID);
-        var optionValue = option.property("value");
-        console.log(optionValue)
-                        
+    //console.log(ID);
+    var option = dropdownMenu.append("option");
+    option.text(ID);
+    // var optionValue = option.property("value");
+    // console.log(optionValue);/(153)
+    
     });
 
-    // //Demographic
-    // const info = data.metadata;
-    // var selectedsample = info.filter(sample => sample.id == option)
-    // info.forEach(function (metadata) {
-    //     //console.log(metadata);
-    //     var row = demographic.append("tr");
-    //     Object.entries(metadata).forEach(function ([key, value]){
-    //         console.log(key,value);
-    //         var cell = row.append("td");
-          
-            
-
-
-    //     })
-
-
-    // });
     
-    // const samples = data.samples;
-    // //console.log(samples);
+
+    //demographic info
+    const metaData = data.metadata;
+    //console.log(metaData);
 });
-
-
-
-
-
-// 
