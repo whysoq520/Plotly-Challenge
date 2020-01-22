b4701edc8f6646c5ead26d3b511ca3f8651143d4
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-01-22 07:33:24 -0500

    organize dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,6 @@
 const path = "../../samples.json";
 // dropdown
 
-
-
 // Assign the dropdownmenu option to a variable
 var dropdownMenu = d3.select("#selDataset");
 const data = d3.json(path).then((data)=>{
@@ -14,9 +12,8 @@ const data = d3.json(path).then((data)=>{
         var option = dropdownMenu.append("option");
         option.text(ID);
                         
-                        });
-           
     });
+});
     
 
 
