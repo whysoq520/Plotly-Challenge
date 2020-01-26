1f32594ec1183b6d1c99105271da30a667cafae6
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-01-26 11:14:46 -0500

    organized code

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,41 +1,27 @@
 var path ="../../samples.json";
 
-// function updateDemography (id) {
-
-//     var initial = d3.select(this).select("option");
-//     var initialValue = initial.property("value");
-//     var initialID =initial.attr("id");
-//     console.log(initialID);
-//     console.log(initialValue);
-// };
-
-
-
-        
-
-
-
 function init() {
 const data = d3.json(path).then(function(data) {    
     //console.log(data);    
 
-    // dropdownmenu ID
+// dropdownmenu ID
+    // setting all samples ID on the dropdownmenu by appending(IDS) to option 
     const names = data.names;
-     // d3.event.preventDefault();
+     //d3.event.preventDefault();
      var dropdownMenu = d3.select("#selDataset");
+
      names.forEach(name => { 
      //console.log(name);
      var option = dropdownMenu.append("option");
      option.text(name);
-     var optionValue = option.property("value");
-     //console.log(optionValue);
- });
+    });
 
 
 
  
  //Demography Info
     var metaData = data.metadata;
+    // applying the first sample metadata to set demographic Info
     var metaData1 =(metaData[0])
         //console.log(metaData[0]);
     var demography = d3.select("#sample-metadata");
@@ -46,6 +32,7 @@ const data = d3.json(path).then(function(data) {
            });
 
 // plots
+    // using first sample to plot
     var samples = data.samples
     //console.log(samples);
     var sample_values = samples.map(sample =>sample.sample_values);  
@@ -55,24 +42,20 @@ const data = d3.json(path).then(function(data) {
     var otu_ids = data.samples.map(sample =>sample.otu_ids); 
     var top10otu_ids = otu_ids[0].slice(0,10)   
     var otu_labels = data.samples.map(sample =>sample.otu_labels);
-    var sampleID = samples.map(sample =>sample.id); 
-    //console.log(sampleID);
-
-
+    
+    //bar plot
     var bardata =[{
         x:top10sample_values.reverse(),
         y:top10otu_ids.map(id =>  ("OTU" + id.toString())),
         type:"bar",
         orientation: "h"
     }];
-
-
     Plotly.newPlot("bar", bardata);  
    
 
 
 
-
+    // bubble plot
     var bubbledata =[{
         x: otu_ids[0],
         y: sample_values[0],
@@ -90,10 +73,8 @@ const data = d3.json(path).then(function(data) {
         xaxis: { title: "OTU ID"}
       };
   
-Plotly.plot("bubble", bubbledata, bubbleLayout);
-
-
-});
+    Plotly.plot("bubble", bubbledata, bubbleLayout);
+ });
 };
 
 
@@ -110,10 +91,10 @@ function optionChanged(ID){
 function updateDemography(ID) {
     d3.json(path).then(function(data){
         var metaData = data.metadata;
-        console.log(metaData);
+        console.log(`MetaData${metaData}`);
         for (var i=0; i<metaData.length; i++) {
             if (metaData[i].id.toString() ===ID) {
-                console.log(metaData[i].id)    
+                //console.log(metaData[i].id)    
                 var demography = d3.select("#sample-metadata");
                 demography.html(" ");
                 Object.entries(metaData[i]).forEach(([key, value]) =>{
@@ -178,15 +159,3 @@ function updatePlots(ID) {
 init();
 
 
-
-// write a function that reacts to if selDataset changes and reruns all the functions to build plots by passing in the argument of the changed sample
-
-
-    //biuldbubble(samples[0]);
-   // buildBar(samples[0])
-    // names.forEach(ID => { 
-    // //console.log(ID);
-    // var option = dropdownMenu.append("option");
-    // option.text(ID);
-    // var optionValue = option.property("value");
-    // console.log(optionValue);/(153)  
